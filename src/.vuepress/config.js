32f892ddc83f28ff4884ec9a99542c2a703b6ede
moorii_mac <36842416+moorii76@users.noreply.github.com>
2019-11-14 01:29:07 +0900

    基本的な設定とページを作成

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,26 @@
 module.exports = {
-  title: 'moorii.net',
-  description: 'my profile website'
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: 'moorii.net',
+      description: 'my profile website'
+    }
+  },
+  head: [['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]],
+  themeConfig: {
+    sidebar: false,
+    search: false,
+    nav: [
+      { text: 'Career', link: '/career' },
+      { text: 'Portfolio', link: '/portfolio' },
+      {
+        text: 'Extra',
+        items: [
+          { text: 'Twitter', link: 'https://twitter.com/moorii_76' },
+          { text: 'GitHub', link: 'https://github.com/moorii76' },
+          { text: 'Qiita', link: 'https://qiita.com/moorii' }
+        ]
+      }
+    ]
+  }
 }
